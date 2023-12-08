dcf3389f0f7043d9b67fe0e8e82de1279ed14549
Aayanjit <aayanjit@ekoinfomatics.com>
2023-12-08 17:42:07 +0530

    change in IP

--- baseLink.js
+++ baseLink.js
@@ -1,8 +1,8 @@
-export const VITE_BASE_LINK = 'http://104.154.120.253:8000/';
+export const VITE_BASE_LINK = 'https://104.154.120.253:8000/';
 // export const VITE_BASE_LINK_2 = 'http://192.168.0.110:8000/';
 // export const VITE_BASE_LINK_2  = 'https://realvedic-api.andaal.com/';
 // export const VITE_BASE_LINK_3 = 'http://192.168.1.16:8000/';
-export const VITE_BASE_LINK_2 = 'http://104.154.120.253:8000/';
+export const VITE_BASE_LINK_2 = 'https://104.154.120.253:8000/';
 // export const VITE_BASE_LINK = 'http://test.vanamamalai.us/';
 
 export const REACT_APP_RAZORPAY_KEY_ID = 'rzp_test_gHJS0k5aSWUMQc';
